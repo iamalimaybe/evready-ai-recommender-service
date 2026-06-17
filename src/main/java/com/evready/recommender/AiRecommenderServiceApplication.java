1ed8529ed760b4585550253355218e5abb2e0fa9
iamalimaybe <52281468+iamalimaybe@users.noreply.github.com>
2026-06-17 19:12:58 +0500

    Create Spring Boot recommender service skeleton

---
+++ src/main/java/com/evready/recommender/AiRecommenderServiceApplication.java
@@ -0,0 +1,13 @@
+package com.evready.recommender;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiRecommenderServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AiRecommenderServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/evready/recommender/AiRecommenderServiceApplicationTests.java
@@ -0,0 +1,11 @@
+package com.evready.recommender;
+
+import org.junit.jupiter.api.Test;
+
+class AiRecommenderServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
